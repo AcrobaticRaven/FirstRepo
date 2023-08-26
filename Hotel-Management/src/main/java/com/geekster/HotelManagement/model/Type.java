b431fd2fedcadeb946170085d175b586d36cf079
Balaji Sengundar <115529777+AcrobaticRaven@users.noreply.github.com>
2023-08-26 14:30:17 +0530

    Delete Hotel-Management directory

--- Hotel-Management/src/main/java/com/geekster/HotelManagement/model/Type.java
+++
@@ -1,6 +0,0 @@
-package com.geekster.HotelManagement.model;
-
-public enum Type {
-    AC,
-    NON_AC
-}
--- Hotel-Management/src/main/java/com/geekster/HotelManagement/repository/IRoomRepo.java
+++
@@ -1,9 +0,0 @@
-package com.geekster.HotelManagement.repository;
-
-import com.geekster.HotelManagement.model.HotelRoom;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRoomRepo extends CrudRepository<HotelRoom,Long> {
-}
